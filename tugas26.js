77149e1c0f5270705ff5cc4ec0d56c7925489737
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-13 21:16:23 +0700

    tugas 26 js

---
+++ tugas26.js
@@ -0,0 +1,18 @@
+function tugas26() {
+    var x = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    var x2 = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16];
+    
+    if (x = false);
+    z = " NOT Infinity";
+
+    y = x2.forEach(function(item){
+
+        console.log("Angka " + item + z);
+
+    });
+
+    return y;
+    
+}
+
+tugas26()
